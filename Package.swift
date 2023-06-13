a21b404a0c69d4f986270dfcbdb0f498e11f42fd
Gong Zhang <zhanggong@me.com>
2023-06-13 10:06:27 +0800

    Add platform requirements

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "UInt128",
+    platforms: [.iOS(.v11), .macOS(.v10_13), .watchOS(.v4)],
     products: [
         .library(name: "UInt128", targets: ["UInt128"]),
     ],
